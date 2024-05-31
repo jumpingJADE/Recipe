f9de827282e0b552298d7ea2e6af6a3771f4fd63
Richard Jin <JinYchao330@gmail.com>
2024-05-31 20:57:01 +1000

    build a structure of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,90 @@
-import React, {useState} from 'react'
-import './App.css'
+import React, { useState } from 'react'
+import { v4 as uuidV4 } from 'uuid'
+import RecipeList from '@/components/Recipe'
+import EditorPanel from '@/components/Editor'
 
-function App() {
-  const [count, setCount] = useState(0)
+const sampleRecipes = [
+  {
+    id: uuidV4(),
+    name: "Plain Chicken",
+    servings: 3,
+    cookTime: "2:45",
+    instructions: [
+      "Put salt on Chicken",
+      "Put chicken in oven",
+      "Eat chicken"
+    ],
+    ingredients: [
+      {
+        id: uuidV4(),
+        name: "Chicken",
+        amount: "2 Pounds"
+      },
+      {
+        id: uuidV4(),
+        name: "Salt",
+        amount: "1 Tbs"
+      }
+    ]
+  },
+  {
+    id: uuidV4(),
+    name: "Plain Pork",
+    servings: 5,
+    cookTime: "0:45",
+    instructions: [
+      "Put paprika on Pork",
+      "Put pork in oven",
+      "Eat pork"
+    ],
+    ingredients: [
+      {
+        id: uuidV4(),
+        name: "Pork",
+        amount: "2 Pounds"
+      },
+      {
+        id: uuidV4(),
+        name: "Paprika",
+        amount: "2 Tbs"
+      }
+    ]
+  },
+  {
+    id: uuidV4(),
+    name: "Plain Apple Pai",
+    servings: 10,
+    cookTime: "3:45",
+    instructions: [
+      "Put apples in pie",
+      "Put pie in oven",
+      "Eat pie"
+    ],
+    ingredients: [
+      {
+        id: uuidV4(),
+        name: "Pork",
+        amount: "2 Pounds"
+      },
+      {
+        id: uuidV4(),
+        name: "Paprika",
+        amount: "2 Tbs"
+      }
+    ]
+  }
+]
 
+const App = () => {
+  const [selectedRecipeId, setSelectedRecipeId] = React.useState()
   return (
     <>
-      <div>APP
-      </div>
+      <RecipeList />
+      {
+        selectedRecipeId && <EditorPanel />
+      }
     </>
+
   )
 }
 
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server:{
+    port: 12345
+  },
+  resolve:{
+    alias:{
+      '@': '/src'
+    }
+  }
 })
