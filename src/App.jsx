e414259fdaf5a8f8ae000da2cc86e99e94e023ed
Richard Jin <JinYchao330@gmail.com>
2024-05-31 20:13:07 +1000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>APP
+      </div>
+    </>
+  )
+}
+
+export default App
